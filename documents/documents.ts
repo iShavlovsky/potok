2a2909c26595fdcf7a3f76bf5d05353999e3ad2c
Vladislav <64088661+alteropt@users.noreply.github.com>
2023-12-20 21:24:24 +0300

    add faq and almost partners

--- documents/documents.ts
+++ documents/documents.ts
@@ -1,6 +1,5 @@
-import '../src/main.scss';
 import './document.scss';
+import '../src/main.scss';
 // import './scripts/cookies';
 // import './scripts/marquee';
 // import './scripts/slider';
-console.log(1);

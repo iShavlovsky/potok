3a5ac2d73e150fa35689b41be9a3378d9aa7c587
Stanislav V <5777227@gmail.com>
2024-01-17 21:56:07 +0300

    add 2 scroll animations

--- src/scripts/indexPage.ts
+++ src/scripts/indexPage.ts
@@ -23,28 +23,48 @@ document.addEventListener('DOMContentLoaded', function () {
   new Marquee('.marquee-2');
 
   /* four Screen-left scrolls */
-  gsap.to('#scrollBlockTriggerLeft', {
-    scrollTrigger: {
-      trigger: '#scrollBlockTrigger',
-      start: 'top top',
-      end: '50%',
-      scrub: 1.5,
-      // markers: true,
-    },
-    duration: 1,
-    xPercent: -90,
+  const mm = gsap.matchMedia();
+
+  mm.add('(min-width: 768px)', () => {
+    // Код анимации, который будет выполнен только при разрешении экрана не ниже 768px
+    gsap.to('#scrollBlockTriggerLeft', {
+      scrollTrigger: {
+        trigger: '#scrollBlockTrigger',
+        start: 'top top',
+        end: '50%',
+        scrub: 1.5,
+      },
+      duration: 1,
+      xPercent: -90,
+    });
+
+    return () => {
+      // Опциональный пользовательский код очистки (выполняется, когда условие больше не соответствует)
+      gsap.killTweensOf('#scrollBlockTriggerLeft');
+    };
   });
 
   /* seven Screen-left scrolls */
-  gsap.to('#scrollBlockTriggerLeft2', {
-    scrollTrigger: {
-      trigger: '#scrollBlockTrigger2',
-      start: 'top top',
-      end: '80%',
-      scrub: 1.5,
-      // markers: true,
-    },
-    duration: 1,
-    xPercent: -80,
+
+  const mm2 = gsap.matchMedia();
+
+  mm2.add('(min-width: 768px)', () => {
+    // Код анимации, который будет выполнен только при разрешении экрана не ниже 768px
+    gsap.to('#scrollBlockTriggerLeft2', {
+      scrollTrigger: {
+        trigger: '#scrollBlockTrigger2',
+        start: 'top top',
+        end: '80%',
+        scrub: 1.5,
+        // markers: true,
+      },
+      duration: 1,
+      xPercent: -80,
+    });
+
+    return () => {
+      // Опциональный пользовательский код очистки (выполняется, когда условие больше не соответствует)
+      gsap.killTweensOf('#scrollBlockTriggerLeft2');
+    };
   });
 });

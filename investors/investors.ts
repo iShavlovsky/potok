0b16b3255bf394a3f2b939d8592bf8e9baaccac1
Stanislav V <5777227@gmail.com>
2024-01-17 23:27:47 +0300

    first screen home and investors animation

--- investors/investors.ts
+++ investors/investors.ts
@@ -1,2 +1,21 @@
 import '../src/main.scss';
 import './investors.scss';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', function () {
+  /* first Screen */
+  gsap.to('#firstScreenContainer2', {
+    scrollTrigger: {
+      trigger: '#firstScreenSection2',
+      start: 'top top',
+      end: 'bottom',
+      scrub: true,
+      pin: '#firstScreenContainer2',
+      // markers: true,
+    },
+    opacity: 0,
+  });
+});

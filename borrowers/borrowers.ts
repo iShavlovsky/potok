37809701d1c616cc2312f7d6ce0e429382e37a7b
Stanislav V <5777227@gmail.com>
2024-01-18 00:26:57 +0300

    add firs screen animation on the borrowers page. And make running stripe on the partners page

--- borrowers/borrowers.ts
+++ borrowers/borrowers.ts
@@ -1,2 +1,21 @@
 import '../src/main.scss';
 import './borrowers.scss';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', function () {
+  /* first Screen */
+  gsap.to('#firstScreenContainer3', {
+    scrollTrigger: {
+      trigger: '#firstScreenSection3',
+      start: 'top top',
+      end: 'bottom',
+      scrub: true,
+      pin: '#firstScreenContainer3',
+      // markers: true,
+    },
+    opacity: 0,
+  });
+});
--- partners/partners.ts
+++ partners/partners.ts
@@ -1,2 +1,12 @@
 import './partners.scss';
 import '../src/main.scss';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Marquee from '../src/scripts/utils/Marguee';
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', function () {
+  /* two Screen - marquee*/
+  new Marquee('.marquee-1');
+  new Marquee('.marquee-2');
+});

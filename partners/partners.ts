1d6e62c78dbceacbdde53a48da473ad743cbed29
Stanislav V <5777227@gmail.com>
2024-01-18 16:59:00 +0300

    fix some bug and make partners animation

--- partners/partners.ts
+++ partners/partners.ts
@@ -22,4 +22,34 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  const benefitsSection = document.querySelector('.benefits-partners-sticy-rail-section');
+  const benefitsBlocks = document.querySelectorAll('.benefits__benefit');
+
+  const sectionHeight = benefitsSection.clientHeight;
+  const blockHeight = sectionHeight / benefitsBlocks.length;
+
+  benefitsBlocks.forEach((block, index) => {
+    ScrollTrigger.create({
+      trigger: benefitsSection,
+      start: `${((index * blockHeight) / sectionHeight) * 100}% top`,
+      end: `${(((index + 1) * blockHeight) / sectionHeight) * 50}% 50%`,
+      onEnter: () => {
+        // Снимаем класс "active" со всех блоков
+        benefitsBlocks.forEach((b) => b.classList.remove('active'));
+
+        // Добавляем класс "active" к текущему блоку
+        block.classList.add('active');
+      },
+      onLeaveBack: () => {
+        // Снимаем класс "active" со всех блоков при возврате скролла назад
+        benefitsBlocks.forEach((b) => b.classList.remove('active'));
+
+        // Добавляем класс "active" к предыдущему блоку при возврате скролла назад
+        if (index > 0) {
+          benefitsBlocks[index - 1].classList.add('active');
+        }
+      },
+    });
+  });
 });

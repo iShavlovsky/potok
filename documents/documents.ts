e1d72258b8552fb96953e04a3512ac9ba5613449
Stanislav V <5777227@gmail.com>
2024-01-19 12:47:58 +0300

    documents page comment script

--- documents/documents.ts
+++ documents/documents.ts
@@ -3,3 +3,28 @@ import './document.scss';
 // import './scripts/cookies';
 // import './scripts/marquee';
 // import './scripts/slider';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const accordionItems = document.querySelectorAll('.accordion__item');
+
+//   accordionItems.forEach((item, index) => {
+//     const title = item.querySelector('.accordion__title');
+
+//     title.addEventListener('click', function () {
+//       // Если аккордион уже открыт, закрываем его
+//       if (item.classList.contains('active')) {
+//         item.classList.remove('active');
+//       } else {
+//         // Закрываем все аккордионы, кроме первого
+//         accordionItems.forEach((otherItem, otherIndex) => {
+//           if (otherIndex !== index) {
+//             otherItem.classList.remove('active');
+//           }
+//         });
+
+//         // Открываем текущий аккордион
+//         item.classList.add('active');
+//       }
+//     });
+//   });
+// });

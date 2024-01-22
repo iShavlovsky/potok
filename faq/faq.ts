c937309bd2578ddc8e503aa8ae53a256242ae288
Stanislav V <5777227@gmail.com>
2024-01-22 07:16:42 +0300

    faq add navbar

--- faq/faq.ts
+++ faq/faq.ts
@@ -15,4 +15,39 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  /* Open navBar */
+  const menuButton = document.querySelector('.header__navigation-menu');
+  const navbar = document.querySelector('.header__navbar');
+  const closeNavbarButton = navbar.querySelector('.close-navbar');
+
+  menuButton.addEventListener('click', () => {
+    // Проверяем разрешение экрана
+    if (window.innerWidth <= 1024) {
+      // Добавляем классы и анимацию
+      navbar.classList.add('active', 'fade-in');
+      document.body.style.overflow = 'hidden'; // Задаем overflow: hidden для body
+    }
+  });
+
+  closeNavbarButton.addEventListener('click', () => {
+    // Убираем класс active и анимацию
+    navbar.classList.remove('active', 'fade-in');
+    document.body.style.overflow = 'visible'; // Восстанавливаем исходный overflow: visible для body
+  });
+
+  /* hide and show navbar */
+  let prevScrollPos = window.pageYOffset;
+
+  window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+      document.querySelector('.header').style.transform = 'translateY(0)';
+    } else {
+      document.querySelector('.header').style.transform = 'translateY(-150px)';
+    }
+
+    prevScrollPos = currentScrollPos;
+  };
 });

31aa5295de59b1fcc194a5599046acb83408cb13
Stanislav V <5777227@gmail.com>
2024-01-22 06:57:23 +0300

    add investors navbar and fix bug

--- investors/investors.ts
+++ investors/investors.ts
@@ -40,4 +40,61 @@ tabs.forEach((tab) => {
       }
     });
   });
+
+  /* Open navBar */
+  const menuButton = document.querySelector('.header__navigation-menu');
+  const navbar = document.querySelector('.header__navbar');
+  const closeNavbarButton = navbar.querySelector('.close-navbar');
+
+  menuButton.addEventListener('click', () => {
+    if (window.innerWidth <= 1024) {
+      navbar.classList.add('active', 'fade-in');
+      document.body.style.overflow = 'hidden';
+    }
+  });
+
+  closeNavbarButton.addEventListener('click', () => {
+    navbar.classList.remove('active', 'fade-in');
+    document.body.style.overflow = 'visible';
+  });
+
+  /* hide and show navbar */
+  let prevScrollPos = window.pageYOffset;
+
+  window.onscroll = function () {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+      document.querySelector('.header').style.transform = 'translateY(0)';
+    } else {
+      document.querySelector('.header').style.transform = 'translateY(-150px)';
+    }
+
+    prevScrollPos = currentScrollPos;
+  };
+
+  /* change navbar collors */
+  const navbarChangeTrigger = document.querySelector('.navbar-change-trigger');
+  const header = document.querySelector('.header');
+
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return rect.top <= window.innerHeight && rect.bottom >= 0;
+  };
+
+  const handleScroll = () => {
+    if (!isElementInViewport(navbarChangeTrigger)) {
+      header.classList.add('transparent');
+    } else {
+      header.classList.remove('transparent');
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  document.addEventListener('DOMContentLoaded', () => {
+    handleScroll();
+  });
+
+  window.addEventListener('resize', handleScroll);
 });
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,3 @@ import './main.scss';
 import './scripts/indexPage';
 // import './scripts/marquee';
 // import './scripts/slider';
-
-/* Open navBar */
-const menuButton = document.querySelector('.header__navigation-menu');
-const navbar = document.querySelector('.header__navbar');
-const closeNavbarButton = navbar.querySelector('.close-navbar');
-
-menuButton.addEventListener('click', () => {
-  if (window.innerWidth <= 1024) {
-    navbar.classList.add('active', 'fade-in');
-    document.body.style.overflow = 'hidden';
-  }
-});
-
-closeNavbarButton.addEventListener('click', () => {
-  navbar.classList.remove('active', 'fade-in');
-  document.body.style.overflow = 'visible';
-});

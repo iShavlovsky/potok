e37ac367f210ca409c09f882a80b977a301d12ad
Stanislav V <5777227@gmail.com>
2024-01-22 07:04:22 +0300

    fix

--- investors/investors.ts
+++ investors/investors.ts
@@ -22,79 +22,79 @@ document.addEventListener('DOMContentLoaded', function () {
 
   /* Our partners Screen - marquee*/
   new Marquee('.marquee-1');
-});
 
-const tabs: NodeListOf<Element> = document.querySelectorAll('.advantages__advantage');
+  const tabs: NodeListOf<Element> = document.querySelectorAll('.advantages__advantage');
 
-tabs.forEach((tab) => {
-  tab.addEventListener('click', function () {
-    const tabNumber: string = this.getAttribute('data-adv') || '1';
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', function () {
+      const tabNumber: string = this.getAttribute('data-adv') || '1';
 
-    // Переключаем класс "active" для всех элементов
-    tabs.forEach((tab) => tab.classList.remove('active'));
+      // Переключаем класс "active" для всех элементов
+      tabs.forEach((tab) => tab.classList.remove('active'));
 
-    // Добавляем класс "active" только к элементам с выбранным data-adv
-    tabs.forEach((tab) => {
-      if (tab.getAttribute('data-adv') === tabNumber) {
-        tab.classList.add('active');
+      // Добавляем класс "active" только к элементам с выбранным data-adv
+      tabs.forEach((tab) => {
+        if (tab.getAttribute('data-adv') === tabNumber) {
+          tab.classList.add('active');
+        }
+      });
+    });
+
+    /* Open navBar */
+    const menuButton = document.querySelector('.header__navigation-menu');
+    const navbar = document.querySelector('.header__navbar');
+    const closeNavbarButton = navbar.querySelector('.close-navbar');
+
+    menuButton.addEventListener('click', () => {
+      if (window.innerWidth <= 1024) {
+        navbar.classList.add('active', 'fade-in');
+        document.body.style.overflow = 'hidden';
       }
     });
+
+    closeNavbarButton.addEventListener('click', () => {
+      navbar.classList.remove('active', 'fade-in');
+      document.body.style.overflow = 'visible';
+    });
+
+    /* hide and show navbar */
+    let prevScrollPos = window.pageYOffset;
+
+    window.onscroll = function () {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        document.querySelector('.header').style.transform = 'translateY(0)';
+      } else {
+        document.querySelector('.header').style.transform = 'translateY(-150px)';
+      }
+
+      prevScrollPos = currentScrollPos;
+    };
+
+    /* change navbar collors */
+    const navbarChangeTrigger = document.querySelector('.navbar-change-trigger');
+    const header = document.querySelector('.header');
+
+    const isElementInViewport = (el) => {
+      const rect = el.getBoundingClientRect();
+      return rect.top <= window.innerHeight && rect.bottom >= 0;
+    };
+
+    const handleScroll = () => {
+      if (!isElementInViewport(navbarChangeTrigger)) {
+        header.classList.add('transparent');
+      } else {
+        header.classList.remove('transparent');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    document.addEventListener('DOMContentLoaded', () => {
+      handleScroll();
+    });
+
+    window.addEventListener('resize', handleScroll);
   });
-
-  /* Open navBar */
-  const menuButton = document.querySelector('.header__navigation-menu');
-  const navbar = document.querySelector('.header__navbar');
-  const closeNavbarButton = navbar.querySelector('.close-navbar');
-
-  menuButton.addEventListener('click', () => {
-    if (window.innerWidth <= 1024) {
-      navbar.classList.add('active', 'fade-in');
-      document.body.style.overflow = 'hidden';
-    }
-  });
-
-  closeNavbarButton.addEventListener('click', () => {
-    navbar.classList.remove('active', 'fade-in');
-    document.body.style.overflow = 'visible';
-  });
-
-  /* hide and show navbar */
-  let prevScrollPos = window.pageYOffset;
-
-  window.onscroll = function () {
-    const currentScrollPos = window.pageYOffset;
-
-    if (prevScrollPos > currentScrollPos) {
-      document.querySelector('.header').style.transform = 'translateY(0)';
-    } else {
-      document.querySelector('.header').style.transform = 'translateY(-150px)';
-    }
-
-    prevScrollPos = currentScrollPos;
-  };
-
-  /* change navbar collors */
-  const navbarChangeTrigger = document.querySelector('.navbar-change-trigger');
-  const header = document.querySelector('.header');
-
-  const isElementInViewport = (el) => {
-    const rect = el.getBoundingClientRect();
-    return rect.top <= window.innerHeight && rect.bottom >= 0;
-  };
-
-  const handleScroll = () => {
-    if (!isElementInViewport(navbarChangeTrigger)) {
-      header.classList.add('transparent');
-    } else {
-      header.classList.remove('transparent');
-    }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  document.addEventListener('DOMContentLoaded', () => {
-    handleScroll();
-  });
-
-  window.addEventListener('resize', handleScroll);
 });

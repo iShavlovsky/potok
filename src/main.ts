e7bc41752cf1a5ae5138b776ce320785dea119ed
Stanislav V <5777227@gmail.com>
2024-01-22 06:52:38 +0300

    add new navbar in partners page

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,19 @@ import './scripts/indexPage';
 // import './scripts/marquee';
 // import './scripts/slider';
 
+/* Open navBar */
 const menuButton = document.querySelector('.header__navigation-menu');
 const navbar = document.querySelector('.header__navbar');
 const closeNavbarButton = navbar.querySelector('.close-navbar');
 
 menuButton.addEventListener('click', () => {
-  // Проверяем разрешение экрана
   if (window.innerWidth <= 1024) {
-    // Добавляем классы и анимацию
     navbar.classList.add('active', 'fade-in');
-    document.body.style.overflow = 'hidden'; // Задаем overflow: hidden для body
+    document.body.style.overflow = 'hidden';
   }
 });
 
 closeNavbarButton.addEventListener('click', () => {
-  // Убираем класс active и анимацию
   navbar.classList.remove('active', 'fade-in');
-  document.body.style.overflow = 'visible'; // Восстанавливаем исходный overflow: visible для body
+  document.body.style.overflow = 'visible';
 });

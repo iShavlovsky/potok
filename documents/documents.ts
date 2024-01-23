bb5d19b348fb7bbbc7968a410191217bb273ce0b
Stanislav V <5777227@gmail.com>
2024-01-23 13:34:27 +0300

    documents TypeScript change

--- documents/documents.ts
+++ documents/documents.ts
@@ -7,55 +7,54 @@ import './document.scss';
 document.addEventListener('DOMContentLoaded', function () {
   const accordionItems = document.querySelectorAll('.accordion__item');
 
+  /* Open accordion */
   accordionItems.forEach((item, index) => {
-    const title = item.querySelector('.accordion__title');
+    const title = item.querySelector('.accordion__title') as HTMLElement;
 
     title.addEventListener('click', function () {
-      // Если аккордион уже открыт, закрываем его
       if (item.classList.contains('active')) {
         item.classList.remove('active');
       } else {
-        // Открываем текущий аккордион
         item.classList.add('active');
       }
     });
   });
 
-/* Open navBar */
-const menuButton = document.querySelector('.header__navigation-menu') as HTMLElement;
-const navbar = document.querySelector('.header__navbar') as HTMLElement;
-const closeNavbarButton = navbar.querySelector('.close-navbar') as HTMLElement;
+  /* Open navBar */
+  const menuButton = document.querySelector('.header__navigation-menu') as HTMLElement;
+  const navbar = document.querySelector('.header__navbar') as HTMLElement;
+  const closeNavbarButton = navbar.querySelector('.close-navbar') as HTMLElement;
 
-menuButton.addEventListener('click', () => {
-  if (window.innerWidth <= 1024) {
-    navbar.classList.add('active', 'fade-in');
-    document.body.style.overflow = 'hidden';
-  }
-});
+  menuButton.addEventListener('click', () => {
+    if (window.innerWidth <= 1024) {
+      navbar.classList.add('active', 'fade-in');
+      document.body.style.overflow = 'hidden';
+    }
+  });
 
-closeNavbarButton.addEventListener('click', () => {
-  navbar.classList.remove('active', 'fade-in');
-  document.body.style.overflow = 'visible';
-});
+  closeNavbarButton.addEventListener('click', () => {
+    navbar.classList.remove('active', 'fade-in');
+    document.body.style.overflow = 'visible';
+  });
 
-    /* hide and show navbar */
-    let prevScrollPos: number = window.pageYOffset;
+  /* hide and show navbar */
+  let prevScrollPos: number = window.pageYOffset;
 
-    window.onscroll = function () {
-      const currentScrollPos: number = window.pageYOffset;
+  window.onscroll = function () {
+    const currentScrollPos: number = window.pageYOffset;
 
-      if (prevScrollPos > currentScrollPos) {
-        const header = document.querySelector('.header') as HTMLElement;
-        if (header) {
-          header.style.transform = 'translateY(0)';
-        }
-      } else {
-        const header = document.querySelector('.header') as HTMLElement;
-        if (header) {
-          header.style.transform = 'translateY(-150px)';
-        }
+    if (prevScrollPos > currentScrollPos) {
+      const header = document.querySelector('.header') as HTMLElement;
+      if (header) {
+        header.style.transform = 'translateY(0)';
       }
+    } else {
+      const header = document.querySelector('.header') as HTMLElement;
+      if (header) {
+        header.style.transform = 'translateY(-150px)';
+      }
+    }
 
-      prevScrollPos = currentScrollPos;
-    };
+    prevScrollPos = currentScrollPos;
+  };
 });
